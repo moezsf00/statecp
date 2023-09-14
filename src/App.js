adff21c0f1e9374212505f1c2cfd6269a54a0d61
moezsf00 <moezsf0060@gmail.com>
2023-09-14 11:21:56 +0100

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state={
+  Person :{
+  fullName:"Cristiano Ronaldo  ",
+  bio:"Cristiano Ronaldo dos Santos Aveiro GOIH ComM is a Portuguese professional footballer who plays as a forward for and captains both Saudi Pro League club Al Nassr and the Portugal national team",
+  imgSrc:"https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcQfniAGFxBnFdtMRmpK8BnTN082bMLIo515fUZjEufqpLw3n9IwjJsaprNwPFsUDgruy8rdjNOJ5WHDulU",
+  profession:"soccer player"  
+  }  ,
+  shows:true
+  }
+  timer=0
+ changeShows=()=>{
+  this.state.shows?this.setState({shows:false}):this.setState({shows:true})
+  
+ }
+  render() {
+    return (
+      <>
+      <div style={this.state.shows?{display:"block"}:{display:"none"}} >
+       <h1>{this.state.Person.fullName}</h1> 
+       <img src={this.state.Person.imgSrc} style={{width:"300px"}} ></img>
+       <p>{this.state.Person.bio}</p>
+       <h1>{this.state.Person.profession}</h1> 
+        
+        
+        </div>      
+      <button onClick= {this.changeShows} >change</button>
+      <h1>{this.timer}</h1>
+      </>
+    )
+  }
 }
 
-export default App;
